22bd5977b9da871a8cdf2bc08049ae2c2b62152a
Arya Krishnamurthy <user@ip-172-31-16-92.ap-south-1.compute.internal>
2025-06-03 21:05:02 +0000

    Feat, created a makefile to build server_side and added a Server.hpp

---
+++ exercise-7/Wattsapp/Server.hpp
@@ -0,0 +1,31 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+#include <string>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+//#include <netdb.h>
+
+#include <fcntl.h>
+ 
+#include <cstring>
+
+#include "Server_channel.hpp"
+#include "Server_client.hpp"
+
+static void epoll_ctl_add(int epfd,int fd,uint32_t events);
+
+static void set_sockaddr(sockaddr_in * addr);
+
+static int set_non_blocking(int socketfd);
+
+void parse_message(std::string& message,Client* sender);
+
+void server_run();
+
+#endif
--- exercise-7/Wattsapp/main.cpp
+++ exercise-7/Wattsapp/main.cpp
@@ -3,9 +3,15 @@
 #include <sys/socket.h>
 #include "Server_channel.hpp"
 #include "Server_client.hpp"
+#include "Database.hpp"
+#include "Server.hpp"
 
-extern std::map<int,Client*> socket_client;
+std::map<int,Client*> socket_client;
 
-extern std::map<std::string,Client*> user_name_client_ptr;
+std::map<std::string,Client*> user_name_client_ptr;
 
-extern std::map<std::string,Channel*> channel_name_channel_ptr;
+std::map<std::string,Channel*> channel_name_channel_ptr;
+
+int main(){
+        server_run();
+}

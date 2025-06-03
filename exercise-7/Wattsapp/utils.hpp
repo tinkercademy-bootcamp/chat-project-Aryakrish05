a553847a46948b8f980047d6bfdb823d762fd54e
Arya Krishnamurthy <user@ip-172-31-16-92.ap-south-1.compute.internal>
2025-06-03 13:58:56 +0000

    Feat, Restructured code into files

--- exercise-7/Wattsapp/utils.hpp
+++ exercise-7/Wattsapp/utils.hpp
@@ -1,21 +1,29 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
-
+#include <iostream>
+#include <string>
+#define PORT 8080
 //predefined starting characters of each communicated message between server and client
 //indicate which character corresponds to which command
 enum class Command:char{
+        
         CREATE_USERNAME='a',
+        
         SEND_INVITE='b',
         REC_INVITE='c',
         ACCEPT_INVITE='d',
+        
         CREATE_CHANNEL='e',
         SEND_MSG_CHANNEL='f',
-        REC_MSG_CHANNEL='g'
+        REC_MSG_CHANNEL='g',
         EXIT_CHANNEL='h',
-        REC_INVITE='i',
-        CHANNEL_EXITED='j',
-        CHANNEL_CREATED='k',
-        TERMIN='\0'
+
+        USER_CREATED='i',
+        USERNAME_IN_USE='j',
+        CHANNEL_EXITED='k',
+        CHANNEL_CREATED='l',
+        
+        TERMINAL='\0'
 };
 
 #endif
